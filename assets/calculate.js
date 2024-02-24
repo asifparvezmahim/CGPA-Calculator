08b2b3a74b25a1d34eb83c5d291f776561a70e5d
Md.Asif Parvez Mahim <100875772+asifparvezmahim@users.noreply.github.com>
2024-02-24 14:03:43 +0600

    second commit

--- assets/calculate.js
+++ assets/calculate.js
@@ -1,4 +1,32 @@
 const addBtn = document.getElementById('add');
+const courseCredit=document.getElementById("credit")
 addBtn.addEventListener("click",()=>{
-        console.log("done");
+  const newForm = document.createElement("form")
+  newForm.classList.add('form')
+  const newField=`
+  <form>              
+  <div class="inputField">
+     <input type="text" placeholder="Course Name" class="CourseNameField" required >
+     <input type="number" placeholder="Credit Units" class="CreditField" value="" id="credit" required>
+     <select class="gradeField" required>
+     <option class="grade" value="select" id="">Select</option>
+     <option class="grade" value="">A+</option>
+     <option class="grade" value="">A</option>
+     <option class="grade" value="">A-</option>
+     <option class="grade" value="">B+</option>
+     <option class="grade" value="">B</option>
+     <option class="grade" value="">B-</option>
+     <option class="grade" value="">C+</option>
+     <option class="grade" value="">C</option>
+     <option class="grade" value="">D</option>
+     <option class="grade" value="">F</option>
+     </select>
+     <div class="deleteBtnWrap"><button class="deleteBtn" id="deleteBtn" name="deleteBtn" value="">-</button></div>             
+  </div>
+
+</form>
+`
+newForm.innerHTML=newField
+document.querySelector(".courseWrapper").appendChild(newForm)
 })
+
